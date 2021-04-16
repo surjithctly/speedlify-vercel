f4c8d4963b93c36327c0c120b960e883db828ca7
Surjith S M <surjithctly@gmail.com>
2021-04-16 21:06:59 +0530

    setup sites

---
+++ _data/sites/personal.js
@@ -0,0 +1,14 @@
+module.exports = {
+	name: "Personal Websites", // optional, falls back to object key
+	description: "Surjith's Personal websites",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://surjithctly.in/",
+    "https://web3canvas.com/",
+		"https://web3forms.com/",
+    "https://web3templates.com/",
+    "https://web3templates.com/components",
+	]
+};
---
+++ _data/sites/templates.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: "Website Templates", // optional, falls back to object key
+	description: "Website Templates",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://logistics-next.netlify.app/",
+		"https://realestate-next.netlify.app/",
+	]
+};
