3a98e66ea2363f06eda9b5c1b3f4c9d58120a133
Surjith S M <surjithctly@gmail.com>
2021-04-22 07:58:12 -0700

    added mission-control.app

--- _data/sites/personal.js
+++ _data/sites/personal.js
@@ -1,14 +1,15 @@
 module.exports = {
-	name: "Personal Websites", // optional, falls back to object key
-	description: "Surjith's Personal websites",
-	options: {
-		frequency: 60 * 23, // (in minutes), 23 hours
-	},
-	urls: [
-		"https://surjithctly.in/",
-    "https://web3canvas.com/",
-		"https://web3forms.com/",
-    "https://web3templates.com/",
-    "https://web3templates.com/components",
-	]
+    name: "Personal Websites", // optional, falls back to object key
+    description: "Surjith's Personal websites",
+    options: {
+        frequency: 60 * 23, // (in minutes), 23 hours
+    },
+    urls: [
+        "https://surjithctly.in/",
+        "https://web3canvas.com/",
+        "https://web3forms.com/",
+        "https://mission-control.app/",
+        "https://web3templates.com/",
+        "https://web3templates.com/components",
+    ]
 };
